36b3c5db97ce5c8ad0218820fea537e4760526b1
Joshua Corvinus <dude.dudeness@gmail.com>
2018-09-19 21:19:32 -0400

    Adding scene switching for demo purposes

---
+++ Assets/_LeapControllerCompatibility/Scripts/Demo/SceneSwitcher.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace CoordinateSpaceConversion
+{
+    public class SceneSwitcher : MonoBehaviour
+    {
+        static SceneSwitcher instance;
+        public static SceneSwitcher Instance { get { return instance; } }
+
+        string[] sceneList = new string[]
+        {
+            "SteamVRTest",
+            "Basic Interactions",
+            "Basic UI",
+            "Dynamic UI",
+            "Hand UI",
+            "Anchors",
+            "Interaction Callbacks",
+            "Moving Reference Frame",
+            "SwapGrasp"
+        };
+
+        int currentSceneIndex = -1;
+
+        private void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+                DontDestroyOnLoad(this.gameObject);
+            }
+            else Destroy(gameObject);
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            Scene scene = SceneManager.GetActiveScene();
+
+            currentSceneIndex = scene.buildIndex;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                currentSceneIndex--;
+                currentSceneIndex = (int)Mathf.Repeat(currentSceneIndex, sceneList.Length);
+                SceneManager.LoadScene(sceneList[currentSceneIndex], LoadSceneMode.Single);
+            }
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                currentSceneIndex++;
+                currentSceneIndex = (int)Mathf.Repeat(currentSceneIndex, sceneList.Length);
+                SceneManager.LoadScene(sceneList[currentSceneIndex], LoadSceneMode.Single);
+            }
+        }
+    }
+}
