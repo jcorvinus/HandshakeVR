6a25b4392f095dd7950fc48ff084dce7a5c78141
Joshua Corvinus <dude.dudeness@gmail.com>
2020-12-13 21:00:43 -0600

    Adding a bunch of stuff

---
+++ Assets/HandshakeVR/Scripts/Avatar/VisibilityOculusHands.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Oculus;
+
+namespace HandshakeVR.Avatar
+{
+	public class VisibilityOculusHands : AvatarVisibility
+	{
+		[SerializeField] Renderer skeletalRenderer;
+		[SerializeField] OVRMeshRenderer ovrRenderer;
+
+		public override void SetVisibility(bool visible)
+		{
+			skeletalRenderer.enabled = visible;
+			ovrRenderer.enabled = visible;
+		}
+	}
+}
