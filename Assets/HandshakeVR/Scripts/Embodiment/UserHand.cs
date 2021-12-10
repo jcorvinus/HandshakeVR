a3a52dc825f17fd90d9ec687f56816c6718ff8d0
Joshua Corvinus <dude.dudeness@gmail.com>
2021-12-10 15:42:43 -0500

    Tracking vars and events for UserHand

--- Assets/HandshakeVR/Scripts/Embodiment/UserHand.cs
+++ Assets/HandshakeVR/Scripts/Embodiment/UserHand.cs
@@ -7,8 +7,34 @@ using Leap.Unity;
 
 namespace HandshakeVR
 {
+	// Just storing some hand-relevant definitions common to all sorts of UI stuff.
+	// note: fingers point 'forward' on the z axis, the red axis points inwards on the hand (from index to thumb) and the green axis points away from the palm)
+
+	public enum HandFilter { Left = 0, Right = 1, Either = 2, Both = 3, None = 4 }
+	[System.Flags]
+	public enum FingerFilter { none = 0, thumb = 0x1, index = 0x2, middle = 0x4, ring = 0x8, pinky = 0x0f }
+	public enum HandLocation { finger, wrist, forearm, palm }
+
+	/// <summary>
+	/// Stores relevant information about a fingertip.
+	/// </summary>
+	public struct FingertipData
+	{
+		public CapsuleCollider Owner;
+		public bool HandLeft { get { return HandModel.IsLeft; } }
+		public FingerFilter finger;
+		public Vector3 TipPosition { get { return Owner.transform.TransformPoint(Owner.center) + Forward * Owner.height; } }
+		public Vector3 Forward { get { return (Owner.transform.forward * ((HandLeft) ? 1 : 1)); } }
+
+		public UserHand HandModel;
+	}
+
 	public class UserHand : MonoBehaviour
 	{
+		public System.Action<UserHand> OnTrackingGained;
+		public System.Action<UserHand> OnTrackingLost;
+
+		bool previousWasTracking = false;
 		UserRig userRig;
 		SkeletalControllerHand skeletalControllerHand;
 		DataHand dataHand;
@@ -17,8 +43,12 @@ namespace HandshakeVR
 		OverridableHandEnableDisable riggedEnabler, rigidEnabler;
 
 		private bool handEnabled = true;
+		bool disableUINonIndexFingertips = false;
 		[SerializeField] bool isLeft;
 		public bool IsLeft { get { return isLeft; } }
+		public bool IsTracked { get { return dataHand.IsTracked; } }
+		public HandInputProvider ActiveInputProvider { get { return skeletalControllerHand.ActiveProvider; } }
+		public bool DisableUINonIndexFingertips { get { return disableUINonIndexFingertips; } }
 
 		// hands can be enabled or disabled so that you can replace them with embodied tools.
 		// disabling a hand will not disable the Data Hand underneath it - this is so that you can
@@ -79,6 +109,23 @@ namespace HandshakeVR
 
 			riggedEnabler = riggedhand.GetComponent<OverridableHandEnableDisable>();
 			rigidEnabler = rigidHand.GetComponent<OverridableHandEnableDisable>();
+		}
+
+		void Update()
+		{
+			if(dataHand.IsTracked != previousWasTracking) // we have a tracking event change
+			{
+				if(dataHand.IsTracked)
+				{
+					if (OnTrackingGained != null) OnTrackingGained(this);
+				}
+				else
+				{
+					if (OnTrackingLost != null) OnTrackingLost(this);
+				}
+			}
+
+			previousWasTracking = dataHand.IsTracked;
 		}
 	}
 }
